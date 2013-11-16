07f17f8e0a2ea9fe2f5d026ae82b38367166acdd
user003 <asifgogo82@yahoo.com>
2013-11-15 22:42:03 -0800

    Create server.rb

---
+++ server.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'rest-client'
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+get '/' do
+  erb :index, :locals => {:client_id => CLIENT_ID}
+end
